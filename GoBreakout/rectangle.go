ee6b85b71ab028adeb32fee4869c39ab3df4c4df
Ian Wigley <ian.wigley@outlook.com>
2023-01-12 09:19:15 +0000

    Update rectangle.go
    Update rectangle.go.

--- GoBreakout/rectangle.go
+++ GoBreakout/rectangle.go
@@ -1,16 +1,26 @@
 package main
 
-// rectangle represents a rectangle for collision detection
-type rectangle struct {
+// Rectangle represents a Rectangle for collision detection
+type Rectangle struct {
 	x      int
 	y      int
 	width  int
 	height int
 }
 
-func (r *rectangle) CheckCollisions() bool {
+func (rectangle *Rectangle) CheckCollisions() bool {
 	if ballRect.x < blockRect.x+blockRect.width && blockRect.x < ballRect.x+ballRect.width && ballRect.y < blockRect.y+blockRect.height {
 		return blockRect.y < ballRect.y+ballRect.height
+	} else {
+		return false
+	}
+}
+
+func (rectangle *Rectangle) Intersects(otherRect *Rectangle) bool {
+	if ballRect.x < otherRect.x+otherRect.width && 
+	otherRect.x < ballRect.x+ballRect.width && 
+	ballRect.y < otherRect.y+otherRect.height {
+		return otherRect.y < ballRect.y+ballRect.height
 	} else {
 		return false
 	}
