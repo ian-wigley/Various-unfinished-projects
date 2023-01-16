0f3060ce8e705c673c036eb68ba990d1bf3c5805
Ian Wigley <ian.wigley@outlook.com>
2023-01-16 09:11:31 +0000

    Update vector2.go
    Update Vector2.go.

--- GoBreakout/vector2.go
+++ GoBreakout/vector2.go
@@ -1,15 +1,28 @@
 package main
 
+import "math"
+
 type Vector2 struct {
-	x      float64
-	y      float64
+	X float64
+	Y float64
 }
 
 func (vector2 *Vector2) Init(x float64, y float64) {
-	vector2.x = x
-	vector2.y = y
+	vector2.X = x
+	vector2.Y = y
 }
 
 func (vector2 *Vector2) Normalize() {
-	// Todo - add code
+	num := 1 / math.Sqrt(vector2.X*vector2.X+vector2.Y*vector2.Y)
+	vector2.X *= num
+	vector2.Y *= num
+}
+
+func (vector2 *Vector2) Zero() {
+	vector2.X = 0
+	vector2.Y = 0
+}
+
+func (vector2 *Vector2) Length() float64 {
+	return math.Sqrt(vector2.X*vector2.X + vector2.Y*vector2.Y)
 }
