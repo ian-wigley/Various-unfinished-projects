830fcab5a94da6167b07f8a852956b98376fd33e
Ian Wigley <ian.wigley@outlook.com>
2023-01-19 12:28:46 +0000

    Update main.zig
    Update main.zig.

--- Zig_Emulator/src/main.zig
+++ Zig_Emulator/src/main.zig
@@ -5,13 +5,16 @@ const cpu = @import("CPU.zig");
 
 pub fn main() anyerror!void {
     var array = try loadRom();
-    std.log.info("Size of the array is: {any}", .{array.len});
-    std.log.info("Array index: {any}", .{array[1]});
+    // std.log.info("Size of the array is: {any}", .{array.len});
+    // std.log.info("Array index: {any}", .{array[1]});
     cpu.CPU.New(array);
 
-    while (true) {
-        cpu.CPU.Run();
+    var crashed = false;
+    while (!crashed) {
+        crashed = cpu.CPU.Run();
+        std.log.info("CPU has crashed. {any}", .{crashed});
     }
+    std.log.info("CPU has crashed. {any}", .{crashed});
 }
 
 fn loadRom() anyerror![]u8 {
@@ -20,7 +23,7 @@ fn loadRom() anyerror![]u8 {
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
-    std.log.info("in_stream: {any}", .{in_stream.context.buf.len});
+    // std.log.info("in_stream: {any}", .{in_stream.context.buf.len});
 
     var count: usize = 0;
     var array: [in_stream.context.buf.len * 4]u8 = undefined;
@@ -31,12 +34,6 @@ fn loadRom() anyerror![]u8 {
         array[count] = b;
         count += 1;
     }
-    std.log.info("array length: {any}", .{array.len});
+    // std.log.info("array length: {any}", .{array.len});
     return &array;
 }
-
-// test "basic test" {
-//     try std.testing.expectEqual(10, 3 + 7);
-// }
-
-
