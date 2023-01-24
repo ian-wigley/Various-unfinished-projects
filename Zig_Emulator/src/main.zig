cf0d9aa1fce2f03c37bb99f2b54ef07c5f080bd0
Ian Wigley <ian.wigley@outlook.com>
2023-01-24 08:30:13 +0000

    Update main.zig
    Update to MAIN.zig.

--- Zig_Emulator/src/main.zig
+++ Zig_Emulator/src/main.zig
@@ -2,19 +2,19 @@ const std = @import("std");
 const mem = std.mem;
 const allocator = std.mem.Allocator;
 const cpu = @import("CPU.zig");
+const print = std.log.info;
 
 pub fn main() anyerror!void {
     var array = try loadRom();
-    // std.log.info("Size of the array is: {any}", .{array.len});
-    // std.log.info("Array index: {any}", .{array[1]});
+    // print("Size of the array is: {any}", .{array.len});
+    // print("Array index: {any}", .{array[1]});
     cpu.CPU.New(array);
 
     var crashed = false;
     while (!crashed) {
         crashed = cpu.CPU.Run();
-        std.log.info("CPU has crashed. {any}", .{crashed});
     }
-    std.log.info("CPU has crashed. {any}", .{crashed});
+    print("CPU has crashed. {any}", .{crashed});
 }
 
 fn loadRom() anyerror![]u8 {
@@ -23,17 +23,17 @@ fn loadRom() anyerror![]u8 {
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
-    // std.log.info("in_stream: {any}", .{in_stream.context.buf.len});
+    // print("in_stream: {any}", .{in_stream.context.buf.len});
 
     var count: usize = 0;
     var array: [in_stream.context.buf.len * 4]u8 = undefined;
 
     while (count < in_stream.context.buf.len * 2) {
         var b = try in_stream.readByte();
-        // std.log.info("rom bytes: {any}", .{b});
+        // print("rom bytes: {any}", .{b});
         array[count] = b;
         count += 1;
     }
-    // std.log.info("array length: {any}", .{array.len});
+    // print("array length: {any}", .{array.len});
     return &array;
 }
