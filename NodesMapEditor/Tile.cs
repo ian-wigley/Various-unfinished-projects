06c03365a287649c9fc01284c2676fa427069554
Ian Wigley <ian.wigley@outlook.com>
2023-01-26 11:33:26 +0000

    Update Tile.cs
    Update Tile.cs.

--- NodesMapEditor/Tile.cs
+++ NodesMapEditor/Tile.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Text.Json.Serialization;
 using Avalonia.Media.Imaging;
 
@@ -22,12 +24,32 @@ namespace MyApp
         //}
 
         [JsonIgnore]
-        public Bitmap BitmapTile { get; set; }
+        public System.Drawing.Bitmap? BitmapTile { get; set; }
 
         public int DX { get; set; }
 
         public int DY { get; set; }
 
         public int TileNumber { get; set; }
+
+        public Avalonia.Rect rect()
+        {
+            return new Avalonia.Rect(DX, DY, 64, 64);
+        }
+
+
+        public Avalonia.Media.Imaging.Bitmap ConvertToAvaloniaBitmap()
+        {
+            System.Drawing.Bitmap bitmapTmp = new System.Drawing.Bitmap(BitmapTile);
+            var bitmapdata = bitmapTmp.LockBits(new Rectangle(0, 0, bitmapTmp.Width, bitmapTmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            Avalonia.Media.Imaging.Bitmap bitmap1 = new Avalonia.Media.Imaging.Bitmap(Avalonia.Platform.PixelFormat.Bgra8888, Avalonia.Platform.AlphaFormat.Premul,
+                bitmapdata.Scan0,
+                new Avalonia.PixelSize(bitmapdata.Width, bitmapdata.Height),
+                new Avalonia.Vector(96, 96),
+                bitmapdata.Stride);
+            bitmapTmp.UnlockBits(bitmapdata);
+            bitmapTmp.Dispose();
+            return bitmap1;
+        }
     }
 }
