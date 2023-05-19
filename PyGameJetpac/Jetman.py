cf06d07f3d1e7e3917612771405d701bd908937e
Ian Wigley <ian.wigley@outlook.com>
2023-05-19 17:32:08 +0100

    Update Jetman.py
    Migrate to Pygame CE.

--- PyGameJetpac/Jetman.py
+++ PyGameJetpac/Jetman.py
@@ -2,20 +2,26 @@ import pygame
 from BaseObject import BaseObject
 
 class jetman(BaseObject):
+
     def __init__(self, x, y, position, image):
-        super().__init__(x, y, position, image, 0, 0, 0)
+        super().__init__(x, y, position, image, 0, 36, 52)
+        self.m_width = 36
+        self.m_height = 52
+        self.images = image
         self.right_facing_image = image
         self.left_facing_image = pygame.transform.flip(image, True, False)
-        self.m_rect = pygame.Rect(self.m_frame * self.m_width, 0, self.m_width, self.m_height)
+
+    def move(self, position):
+        self.position = position
 
     def update(self):
-        self.m_rect = pygame.Rect(self.m_frame * self.m_width, 0, self.m_width, self.m_height)
+        self.rect.move_ip(self.position.x, self.position.y)
 
-    def draw(self, screen, facing = "right"):
-        if facing == "right":
-            screen.blit(self.right_facing_image, (self.m_x, self.m_y, 600, 800), (0,0,36,52))
-        elif facing == "left":
-            screen.blit(self.left_facing_image, (self.m_x, self.m_y, 600, 800), (146,0,36,52))
+    # def draw(self, screen, facing = "right"):
+    #     if facing == "right":
+    #         screen.blit(self.right_facing_image, (self.m_x, self.m_y, 600, 800), (0,0,36,52))
+    #     elif facing == "left":
+    #         screen.blit(self.left_facing_image, (self.m_x, self.m_y, 600, 800), (146,0,36,52))
 
     def JetmanRect(self):
         return pygame.Rect(self.m_x, self.m_y, 36, 52)
