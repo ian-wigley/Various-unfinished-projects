54c33816b1b1e458cf49a1690bf5df3a3976a582
Ian Wigley <ian.wigley@outlook.com>
2023-05-23 19:00:42 +0100

    Update jet_man.py

--- PyGameJetpac/jet_man.py
+++ PyGameJetpac/jet_man.py
@@ -12,19 +12,21 @@ class jetman(BaseObject):
         self.left_facing_image = pygame.transform.flip(image, True, False)
 
     def move(self, position):
-        self.position = position
+        self.m_position = position
+        self.m_x += position.x
+        self.m_y += position.y
 
     def update(self):
-        self.rect.move_ip(self.position.x, self.position.y)
-
-    # def draw(self, screen, facing = "right"):
-    #     if facing == "right":
-    #         screen.blit(self.right_facing_image, (self.m_x, self.m_y, 600, 800), (0,0,36,52))
-    #     elif facing == "left":
-    #         screen.blit(self.left_facing_image, (self.m_x, self.m_y, 600, 800), (146,0,36,52))
+        self.rect.move_ip(self.m_position.x, self.m_position.y)
+        # def draw(self, screen, facing = "right"):
+        #     if facing == "right":
+        #         screen.blit(self.right_facing_image, (self.m_x, self.m_y, 600, 800), (0,0,36,52))
+        #     elif facing == "left":
+        #         screen.blit(self.left_facing_image, (self.m_x, self.m_y, 600, 800), (146,0,36,52))
 
     def jetman_rect(self):
         return pygame.Rect(self.m_x, self.m_y, 36, 52)
-
-    def jetman_position(self):
-        return self.m_position
+    @property
+    def jetman_position(self) -> pygame.Vector2:
+        return pygame.Vector2(self.m_x, self.m_y)
+        #return self.m_position
