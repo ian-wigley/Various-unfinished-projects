05683e77549779bef7ee3abb84254b858987ca0d
Ian Wigley <ian.wigley@outlook.com>
2023-05-25 18:33:42 +0100

    Update rocket.py

--- PyGameJetpac/rocket.py
+++ PyGameJetpac/rocket.py
@@ -4,16 +4,7 @@ from base_object import BaseObject
 class rocket(BaseObject):
     def __init__(self, x, y, position, image, frame, width, height):
         super().__init__(x, y, position, image, frame, width, height)
-
-#         public Rocket(int x, int y, Texture2D image, int frame, int width, int height)
-#         {
-#             m_image = image;
-#             m_frame = frame;
-#             m_width = width;
-#             m_height = height;
-#             m_rect = new Rectangle(m_frame * m_width, 0, m_width, m_height);
-#             m_screenLocation = new Vector2(x, y);
-#         }
+        self.rect = pygame.Rect(position.x, position.y, image.get_width(), image.get_height())
 
     def Update(self, x, y):
         self.m_position.x = x
