74a275f11748964f6eae84cdd5344245fd643c21
Ian Wigley <ian.wigley@outlook.com>
2023-05-27 19:22:56 +0100

    Update explosion.py

--- PyGameJetpac/explosion.py
+++ PyGameJetpac/explosion.py
@@ -8,15 +8,7 @@ class explosion(BaseObject):
         self.m_height = image.get_height()
         self.m_width = image.get_width() / 16
 
-#         public Explosion(int x, int y, Texture2D image)
-#         {
-#             m_image = image;
-#             m_height = image.Height;
-#             self.m_width = image.Width / 16;
-#             m_screenLocation = new Vector2(x, y);
-#         }
-
-    def Update(self):
+    def update(self)-> None:
         self.m_rect = pygame.Rect(self.m_frame * self.m_width, 0, self.m_width, self.m_height)
         self.m_delay_counter += self.m_elapsed_counter
         if (self.m_delay_counter > 2.2):
@@ -26,5 +18,5 @@ class explosion(BaseObject):
             else:
                 self.m_animationComplete = True
 
-    def AnimationComplete(self):
+    def animation_complete(self):
         return self.m_animationComplete
