fa270426ecc7d93c78a185ebc683f79050a39edb
Ian Wigley <ian.wigley@outlook.com>
2023-05-27 19:22:43 +0100

    Update bonus.py

--- PyGameJetpac/bonus.py
+++ PyGameJetpac/bonus.py
@@ -9,21 +9,21 @@ class bonus(BaseObject):
         self.m_prevFrame = 0
         self.m_width = image.get_width() / 5
         self.m_height = image.get_height() + 2
-        self.m_rect = pygame.Rect(self.m_frame * self.m_width, 0, self.m_width, self.m_height)
-        self.m_position = pygame.Vector2(random.randint(0, 750), random.randint(0, 0))
+        self.rect = pygame.Rect(position.x, position.y, image.get_width(), image.get_height())
+        self.position = pygame.Vector2(random.randint(0, 750), random.randint(0, 0))
 
-    def Update(self):
+    def update(self) -> None:
         if (not self.bonusLanded):
             self.m_position.y += 1
         self.m_rect = pygame.Rect(self.m_frame * self.m_width, 0, self.m_width, self.m_height)
 
-    def BonRect(self):
+    def bonrect(self):
         return pygame.Rect(self.m_position.x, self.m_position.y, self.m_width, self.m_height)
 
     def bonus_landed(self, value):
         self.bonusLanded = value
 
-    def Reset(self):
+    def reset(self):
         self.m_prevFrame = self.m_frame
         self.m_frame = random.randint(0, 4)
         if (self.m_frame == self.m_prevFrame):
