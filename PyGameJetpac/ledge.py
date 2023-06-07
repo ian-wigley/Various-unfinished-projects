f8361774752b7849ed0d7c7a4a736c669afcc04a
Ian Wigley <ian.wigley@outlook.com>
2023-06-07 18:55:22 +0100

    Update ledge.py

--- PyGameJetpac/ledge.py
+++ PyGameJetpac/ledge.py
@@ -6,5 +6,5 @@ class ledge(BaseObject):
         super().__init__(x, y, position, image, frame, width, height)
         self.rect = pygame.Rect(position.x, position.y, image.get_width(), image.get_height())
 
-    def ledge_rect(self):
+    def ledge_rect(self) -> pygame.Rect:
         return pygame.Rect(self.m_position.x, self.m_position.y, self.image.get_width(), self.image.get_height())
