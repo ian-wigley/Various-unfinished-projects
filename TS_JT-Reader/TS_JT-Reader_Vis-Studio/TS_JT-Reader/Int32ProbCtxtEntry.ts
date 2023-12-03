47a637b188d7ce1e6a0eeac4885d734685bfd521
Ian Wigley <ian.wigley@outlook.com>
2023-12-03 17:42:11 +0000

    Create Int32ProbCtxtEntry.ts

---
+++ TS_JT-Reader/TS_JT-Reader_Vis-Studio/TS_JT-Reader/Int32ProbCtxtEntry.ts
@@ -0,0 +1,31 @@
+﻿module C_sharp_JT_Reader {
+    export class Int32ProbCtxtEntry {
+        m_iSym: number;
+        m_cCount: number;
+        m_cCumCount: number;
+        m_iNextCntx: number = 0;
+        m_iAssociatedVal: number = 0;
+        constructor(symbol: number, count: number, associatedVal: number, cumulativeCount: number, nextContext: number) {
+            this.m_iSym = symbol;
+            this.m_cCount = count;
+            this.m_cCumCount = cumulativeCount;
+            this.m_iAssociatedVal = associatedVal;
+            this.m_iNextCntx = nextContext;
+        }
+        public getCumCount(): number {
+            return this.m_cCumCount;
+        }
+        public getOccCount(): number {
+            return this.m_cCount;
+        }
+        public getSymbol(): number {
+            return this.m_iSym;
+        }
+        public getAssociatedValue(): number {
+            return this.m_iAssociatedVal;
+        }
+        public getNextContext(): number {
+            return this.m_iNextCntx;
+        }
+    }
+}
