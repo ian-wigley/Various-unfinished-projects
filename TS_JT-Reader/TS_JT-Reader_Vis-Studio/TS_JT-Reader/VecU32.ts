e730eb83130807321be4b4b67fcc0ef9f900a0ba
Ian Wigley <ian.wigley@outlook.com>
2023-12-03 17:43:46 +0000

    Create VecU32.ts

---
+++ TS_JT-Reader/TS_JT-Reader_Vis-Studio/TS_JT-Reader/VecU32.ts
@@ -0,0 +1,12 @@
+﻿module C_sharp_JT_Reader {
+    export class VecU32 {
+        private _counter: Int32 = 0;
+        private _data: UInt32[];
+        public setLength(size: number): void {
+            this._data = new Array(size);
+        }
+        public ptr(): number {
+            return this._counter++;
+        }
+    }
+}
