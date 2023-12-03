2a7e1922bb583c533ece6a59cca498351b065865
Ian Wigley <ian.wigley@outlook.com>
2023-12-03 17:36:29 +0000

    Create InstanceNodeElement.ts

---
+++ TS_JT-Reader/TS_JT-Reader_Vis-Studio/TS_JT-Reader/InstanceNodeElement.ts
@@ -0,0 +1,20 @@
+﻿module C_sharp_JT_Reader.Nodes {
+    export class InstanceNodeElement extends C_sharp_JT_Reader.GroupJTNode {
+        private m_textBox: List<string> = new List<string>();
+        constructor(fileVersion: number, richTextBox: List<string>) {
+            _fileVersion = fileVersion;
+            this.m_textBox = richTextBox;
+        }
+        public populateData(uncompressed: number[], filePosCount: number): number {
+            this.m_textBox.Add("\n\n---------------------------------- Instance Node Data ----------------------------------");
+            _uncompressed = uncompressed;
+            _filePosCount = filePosCount;
+            _filePosCount = TraverseBaseNodeData();
+            Buffer.BlockCopy(_uncompressed, _filePosCount, fileBytes, 0, 4);
+            var _childNodeObjectID: Int32 = DataTypes.getInt32(fileBytes);
+            this.m_textBox.Add("\nChild Count = " + _childNodeObjectID.ToString());
+            _filePosCount += 4;
+            return this._filePosCount;
+        }
+    }
+}
