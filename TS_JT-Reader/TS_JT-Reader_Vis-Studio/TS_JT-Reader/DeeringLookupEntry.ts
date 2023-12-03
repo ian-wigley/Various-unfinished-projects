ee48c4febccf220c5bf5fff31438d1ba1e14a02b
Ian Wigley <ian.wigley@outlook.com>
2023-12-03 17:35:26 +0000

    Create DeeringLookupEntry.ts

---
+++ TS_JT-Reader/TS_JT-Reader_Vis-Studio/TS_JT-Reader/DeeringLookupEntry.ts
@@ -0,0 +1,26 @@
+﻿module C_sharp_JT_Reader.Codecs {
+    export class DeeringLookupEntry {
+        cosTheta: number;
+        sinTheta: number;
+        cosPsi: number;
+        sinPsi: number;
+        constructor(cosTheta: number, sinTheta: number, cosPsi: number, sinPsi: number) {
+            this.cosTheta = cosTheta;
+            this.sinTheta = sinTheta;
+            this.cosPsi = cosPsi;
+            this.sinPsi = sinPsi;
+        }
+        public getCosTheta(): number {
+            return this.cosTheta;
+        }
+        public getSinTheta(): number {
+            return this.sinTheta;
+        }
+        public getCosPsi(): number {
+            return this.cosPsi;
+        }
+        public getSinPsi(): number {
+            return this.sinPsi;
+        }
+    }
+}
