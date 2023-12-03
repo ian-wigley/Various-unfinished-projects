b4fd801ee2ee5c930f9b5c30068fd561e3f8bb95
Ian Wigley <ian.wigley@outlook.com>
2023-12-03 17:35:06 +0000

    Create DeeringCode.ts

---
+++ TS_JT-Reader/TS_JT-Reader_Vis-Studio/TS_JT-Reader/DeeringCode.ts
@@ -0,0 +1,14 @@
+﻿module C_sharp_JT_Reader.Codecs {
+    export class DeeringCode {
+        m_sextant: number = 0;
+        m_octant: number = 0;
+        m_theta: number = 0;
+        m_psi: number = 0;
+        constructor(sextant: number, octant: number, theta: number, psi: number) {
+            this.m_sextant = sextant;
+            this.m_octant = octant;
+            this.m_theta = theta;
+            this.m_psi = psi;
+        }
+    }
+}
