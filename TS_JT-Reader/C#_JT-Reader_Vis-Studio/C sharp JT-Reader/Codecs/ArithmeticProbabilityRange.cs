a50d68197d99ab5198f325884588f7de38f9be61
Ian Wigley <ian.wigley@outlook.com>
2023-12-06 18:08:26 +0000

    Create ArithmeticProbabilityRange.cs

---
+++ TS_JT-Reader/C#_JT-Reader_Vis-Studio/C sharp JT-Reader/Codecs/ArithmeticProbabilityRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_sharp_JT_Reader.Codecs
+{
+    public class ArithmeticProbabilityRange
+    {
+        private long m_low;
+        private long m_high;
+        private long m_scale;
+
+        public ArithmeticProbabilityRange(long low, long high, long scale)
+        {
+            this.m_low = low;
+            this.m_high = high;
+            this.m_scale = scale;
+        }
+
+        public long getLow()
+        {
+            return this.m_low;
+        }
+
+        public long getHigh()
+        {
+            return this.m_high;
+        }
+
+        public long getScale()
+        {
+            return this.m_scale;
+        }
+    }
+}
