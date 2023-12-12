d76715ee9da01a9b5f4d42ee724d424221bb0aef
Ian Wigley <ian.wigley@outlook.com>
2023-12-12 18:35:37 +0000

    Create Vec3D.cs

---
+++ TS_JT-Reader/C#_JT-Reader_Vis-Studio/C sharp JT-Reader/DataType/Vec3D.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_JT_Reader.DataType
+{
+    public class Vec3D
+    {
+        double x, y, z;
+
+        public Vec3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+        public double getZ()
+        {
+            return z;
+        }
+
+        public float getXf()
+        {
+            return (float)x;
+        }
+
+        public float getYf()
+        {
+            return (float)y;
+        }
+
+        public float getZf()
+        {
+            return (float)z;
+        }
+
+
+    }
+}
