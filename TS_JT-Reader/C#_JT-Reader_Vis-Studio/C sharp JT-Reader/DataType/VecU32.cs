7d27004c35dfa7faf1876512057493f2b5bba137
Ian Wigley <ian.wigley@outlook.com>
2023-12-18 18:25:42 +0000

    Create VecU32.cs

---
+++ TS_JT-Reader/C#_JT-Reader_Vis-Studio/C sharp JT-Reader/DataType/VecU32.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C_sharp_JT_Reader
+{
+    public class VecU32
+    {
+        private Int32 _counter = 0;
+        private UInt32[] _data;
+
+        // Method to set the length of the array
+        public void setLength(int size)
+        {
+            _data = new UInt32[size];
+        }
+
+        // Method to return a pointer to the next available slot in the array
+        public int ptr()
+        {
+            return _counter++;
+        }
+    }
+}
