17168f24e86fae35391154af9cc753a2af154a68
Ian Wigley <ian.wigley@outlook.com>
2023-12-20 09:13:29 +0000

    Create QuantizationParametersDataCollection.cs

---
+++ TS_JT-Reader/C#_JT-Reader_Vis-Studio/C sharp JT-Reader/DataType/QuantizationParametersDataCollection.cs
@@ -0,0 +1,24 @@
+﻿/////////////////////////////////////////////////////////////////////
+//
+// This class contains the Quantization Parameters Data Collection
+//
+// Refer to ? Quantization Parameters Data Collection page ? - fig 30 (Ver 8.1 rev D)
+//
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C_sharp_JT_Reader
+{
+    public class QuantizationParametersDataCollection
+    {
+            public QuantizationParametersDataCollection()
+            {
+            }
+    }
+}
