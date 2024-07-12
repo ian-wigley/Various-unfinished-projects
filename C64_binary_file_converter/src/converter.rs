bad4fa8b19dd044f44adfdfa0c2e6b4d5b9764d7
Ian Wigley <ian.wigley@outlook.com>
2024-07-12 19:42:34 +0100

    Update converter.rs

--- C64_binary_file_converter/src/converter.rs
+++ C64_binary_file_converter/src/converter.rs
@@ -33,6 +33,7 @@ pub mod con {
 
         pub(crate) fn init(&mut self) {
             // Populate the OpCode Map
+            self.opcode.load_xml("Assets/6502-codes.xml");
             self.opcodes = self.opcode.populate_opcodes();
             // Load the external binary file
             self.file_content = self.load_bin_file().unwrap().clone();
