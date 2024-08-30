99551beb76299e6b7f572d4ca53d4a0760e3f340
Ian Wigley <ian.wigley@outlook.com>
2024-08-30 17:56:18 +0100

    Create stars.zig

---
+++ WIP_Zig_Stuff/ZigStars/src/stars.zig
@@ -0,0 +1,40 @@
+pub const Star =  struct {
+    // speed: f64,
+    // angle: f64,
+    x: f64,
+    y: f64,
+    // // colour: String,
+    // width: f64,
+    // height: f64,
+
+
+    // pub fn new(x: f64, y: f64) Star {
+    //     return Star {
+    //         // .speed= 0.0,
+    //         // .angle = 360.0,
+    //         .x = 0,
+    //         .y = 0,
+    //         //.colour = colour,
+    //         // .width = x,
+    //         // .height = y,
+    //     };
+    // }
+
+};
+
+
+
+
+// pub fn update() {
+// self.speed += 0.1;
+// self.angle += 0.025;
+// self.x = f64::cos(self.angle) * self.speed + self.x;
+// self.y = f64::sin(self.angle) * self.speed + self.y;
+// if self.x < 0.0 || self.x > self.width || self.y < 0.0 || self.y > self.height {
+// self.angle = 360.0 * rng.gen::<f64>();
+// self.speed = 0.01;
+// self.x = self.width / 2.0;
+// self.y = self.height / 2.0;
+// }
+// }
+// }
