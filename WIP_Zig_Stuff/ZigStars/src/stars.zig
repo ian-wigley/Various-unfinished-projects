c3c8d29201b8f8f42b8bf8f54a37658d1c81ebe8
Ian Wigley <ian.wigley@outlook.com>
2025-02-03 18:24:40 +0000

    Update stars.zig

--- WIP_Zig_Stuff/ZigStars/src/stars.zig
+++ WIP_Zig_Stuff/ZigStars/src/stars.zig
@@ -1,7 +1,7 @@
 pub const Star = struct {
     // speed: f64,
-    a: u16,
-    b: u64,
+    x: c_int,
+    y: c_int,
     c: u16,
     // angle: f64,
     // x: f64,
@@ -11,11 +11,11 @@ pub const Star = struct {
     // height: f64,
 
     pub fn new(x: u16, y: u16) Star {
-        return Star{
+        return Star {
             // .speed= 0.0,
             //  .angle = 360.0,
-            .a = x,
-            .b = 1,
+            .x = x,
+            .y = y,
             .c = y,
             //.colour = colour,
             // .width = x,
@@ -24,7 +24,9 @@ pub const Star = struct {
     }
 
     pub fn update(self: *Star) void {
-        self.a += 1;
+
+        self.x += 10;
+        self.y += 0;
         // self.speed += 0.1;
         // self.angle += 0.025;
         // self.x = f64::cos(self.angle) * self.speed + self.x;
