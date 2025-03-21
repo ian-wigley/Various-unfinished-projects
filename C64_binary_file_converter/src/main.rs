169a3e07a4035b012c41de2b2e07b76caa86cb4c
Ian Wigley <ian.wigley@outlook.com>
2025-03-21 18:16:57 +0000

    Update main.rs

--- C64_binary_file_converter/src/main.rs
+++ C64_binary_file_converter/src/main.rs
@@ -18,9 +18,13 @@ mod opcode;
 
 #[derive(Copy, Clone)]
 pub enum Message {
-    Changed,
+    Open,
     SaveAs,
+    Quit,
 }
+
+// https://github.com/fltk-rs/fltk-rs/blob/master/fltk/examples/editor2.rs
+// https://fltk-rs.github.io/fltk-book/Menus.html
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "full");
@@ -34,15 +38,29 @@ fn main() {
     let left_display: TextDisplay = TextDisplay::new(5, 35, 400, 550, None);
     let right_display: TextDisplay = TextDisplay::new(415, 35, 400, 550, None);
 
-    let (s, _r) = app::channel::<Message>();
+    let (s, r) = app::channel::<Message>();
     let mut menu: menu::SysMenuBar = menu::SysMenuBar::default().with_size(800, 35);
     menu.set_frame(FrameType::FlatBox);
+    menu.add_emit(
+        "&File/Open...\t",
+        Shortcut::Ctrl | 'o',
+        menu::MenuFlag::Normal,
+        s,
+        Message::Open,
+    );
     menu.add_emit(
         "&File/Save as...\t",
         Shortcut::Ctrl | 'w',
         menu::MenuFlag::Normal,
         s,
         Message::SaveAs,
+    );
+    menu.add_emit(
+        "&File/Quit\t",
+        Shortcut::Ctrl | 'q',
+        menu::MenuFlag::Normal,
+        s,
+        Message::Quit,
     );
     let mut but: Button = Button::new(380, 590, 80, 20, "Add labels");
 
@@ -62,6 +80,15 @@ fn main() {
     });
     but.activate();
 
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            match msg {
+                Message::Open => println!("Open selected"),
+                Message::SaveAs => println!("SaveAs  selected"),
+                Message::Quit => app.quit(),
+            }
+        }
+    }
     app.run().unwrap();
 }
 
