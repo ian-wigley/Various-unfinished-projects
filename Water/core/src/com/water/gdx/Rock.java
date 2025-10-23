555b4cb3c5e75831ccd022b3d0ef2c0bb40184f8
Ian Wigley <ian.wigley@outlook.com>
2025-10-23 15:03:45 +0100

    Code Tidy

--- Water/core/src/com/water/gdx/Rock.java
+++ Water/core/src/com/water/gdx/Rock.java
@@ -1,6 +1,5 @@
 package com.water.gdx;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -10,8 +9,7 @@ public class Rock {
     public Vector2 Position, Velocity;
     static final Vector2 Gravity = new Vector2(0, 0.3f);
 
-    public Rock(Vector2 Position, Vector2 Velocity)
-    {
+    public Rock(Vector2 Position, Vector2 Velocity) {
         this.Position = Position;
         this.Velocity = Velocity;
     }
@@ -28,8 +26,6 @@ public class Rock {
     }
 
     public void Draw(SpriteBatch spriteBatch, Texture texture) {
-        Vector2 origin = new Vector2(texture.getWidth()/2, texture.getHeight()/2);// / 2f;
-        //spriteBatch.draw(texture, Position.x, Position.y);//, null, Color.WHITE, 0f, origin, 1f, 0, 0);
         spriteBatch.draw(texture, Position.x, Position.y, texture.getWidth(), texture.getHeight(), 0, 0, texture.getWidth(),
                 texture.getHeight(), false, true);
     }
