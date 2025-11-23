8925e805ce8860ccd128e890deb08576d7241bd6
Ian Wigley <ian.wigley@outlook.com>
2025-11-23 18:24:49 +0000

    Update Bonus.cs

--- JetpacReloaded/C# version/JetpacReloaded/Bonus.cs
+++ JetpacReloaded/C# version/JetpacReloaded/Bonus.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace JetpacReloaded
+namespace JetPacReloaded
 {
-    class Bonus : BaseObject
+    internal class Bonus : BaseObject
     {
-        private bool bonusLanded = false;
-        private int m_prevFrame = 0;
+        private bool _bonusLanded = false;
+        private int _mPrevFrame = 0;
 
         public Bonus(Texture2D image)
         {
@@ -19,40 +19,34 @@ namespace JetpacReloaded
 
         public void Update()
         {
-            if (!bonusLanded)
+            if (!_bonusLanded)
             {
                 m_screenLocation.Y++;
             }
             m_rect = new Rectangle(m_frame * m_width, 0, m_width, m_height);
         }
 
-        new public void Draw(SpriteBatch spriteBatch)
+        public new void Draw(SpriteBatch spriteBatch)
         {
             Vector2 bonusLocation = new Vector2(m_screenLocation.X, m_screenLocation.Y);
             spriteBatch.Draw(m_image, bonusLocation, m_rect, Color.White, 0, Vector2.Zero, 1.0f, 0, 0);
         }
 
-        public Rectangle BonRect
-        {
-            get
-            {
-                return new Rectangle((int)m_screenLocation.X, (int)m_screenLocation.Y, m_width, m_height);
-            }
-        }
+        public Rectangle BonRect => new((int)m_screenLocation.X, (int)m_screenLocation.Y, m_width, m_height);
 
-        public bool BonusLanded { set { bonusLanded = value; } }
+        public bool BonusLanded { set => _bonusLanded = value; }
 
         public void Reset()
         {
-            m_prevFrame = m_frame;
+            _mPrevFrame = m_frame;
             m_frame = rand.Next(0, 4);
-            if (m_frame == m_prevFrame)
+            if (m_frame == _mPrevFrame)
             {
                 m_frame = (m_frame + 1) % 4;
             }
             m_screenLocation.X = rand.Next(0, 750);
             m_screenLocation.Y = -30;
-            bonusLanded = false;
+            _bonusLanded = false;
         }
     }
 }
